312cf258413e0990026fba7634d8693df9f6abd0
Abhinev <210160307008.abhinev@gdgu.org>
2023-02-16 18:24:54 +0530

    practical of hooks,getting and altering data from server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
 
-function App() {
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  const toggleImportanceOf = (id) => {
+    console.log('importance of ' + id + ' needs to be toggled')
+  }
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
+
+  console.log('render', notes.length, 'notes')
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      // id: notes.length + 1,
+    }
+    // setNotes(notes.concat(noteObject))
+    // setNewNote('')
+    
+    axios
+    .post('http://localhost:3001/notes', noteObject)
+    .then(response => {
+      setNotes(notes.concat(response.data))
+      setNewNote('')
+    }) 
+ }
+
+const handleNoteChange = (event) => {
+  console.log(event.target.value)
+  setNewNote(event.target.value)
+}
+
+const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Notes</h1>
+      <div>
+      <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
+      <ul>
+      {notesToShow.map(note =>
+        <Note 
+        key={note.id} 
+        note={note} 
+          toggleImportance={() => toggleImportanceOf(note.id)}
+        />
+      )}
+      </ul>
+      <form onSubmit={addNote}>
+      <input
+      value={newNote} 
+      onChange={handleNoteChange}
+      />
+        <button type="submit">save</button>
+      </form>   
     </div>
-  );
+  )
 }
 
 export default App;
